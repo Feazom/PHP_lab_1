dec82b13e7d65aad946b112135f7f7e7803b4891
wealbom <bayastanov1an@gmail.com>
2021-10-09 17:10:25 +0500

    added MagicClass

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
 require 'vendor/autoload.php';
+
+$object = new \App\MagicClass();
+$object->method();
+\App\MagicClass::staticMethod();
+$a = $object->variable;
+$object->variable = 12;
+isset($object->variable);
+unset($object->variable);
+$str = serialize($object);
+$object2 = unserialize($str);
+strval($object);
+$object(12);
+var_export($object);
+$object3 = clone $object;
+var_dump($object);
